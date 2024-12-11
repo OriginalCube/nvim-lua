08565207037808010ed5de4f4c765455088fc84c
OriginalCube <lancemiraflorcube@gmail.com>
2024-12-11 13:27:22 +0800

    fix: remove wrong plugin in auto pair

--- lua/plugins/auto-pair.lua
+++ lua/plugins/auto-pair.lua
@@ -1,111 +1,17 @@
 return {
-	-- nvim-cmp (core plugin)
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-			"hrsh7th/cmp-buffer", -- Buffer source
-			"hrsh7th/cmp-path", -- File path source
-			"hrsh7th/cmp-cmdline", -- Command line source
-			"L3MON4D3/LuaSnip", -- Snippets plugin
-			"saadparwaiz1/cmp_luasnip" -- LuaSnip source for nvim-cmp
-		},
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- Use LuaSnip for snippets
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept selected item
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" }, -- LSP completion source
-					{ name = "luasnip" }, -- Snippets source
-				}, {
-					{ name = "buffer" }, -- Buffer source
-					{ name = "path" }, -- Path source
-				})
-			})
-
-			-- Setup for command line completion
-			cmp.setup.cmdline("/", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-			})
-		end
-	},
-
-	-- LuaSnip (required for snippets support)
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = { "rafamadriz/friendly-snippets" },
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end
-	},
-
-	-- null-ls integration
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.diagnostics.eslint,
-					null_ls.builtins.completion.spell,
-				},
-			})
-		end
-	},
-
-	-- nvim-lspconfig integration
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			-- Example LSP server setup
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-
-			-- Volar (Vue) integration
-			lspconfig.volar.setup({
-				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-				capabilities = capabilities,
-				settings = {
-					volar = {
-						codeLens = {
-							references = true,
-							pugTools = true,
-						},
-					},
-				},
-			})
-		end
-	}
+	"windwp/nvim-autopairs",
+	config = function()
+		require("nvim-autopairs").setup({
+			check_ts = true,
+			ts_config = {
+				lua = { "string", "source" },
+				javascript = { "string", "template_string" },
+			},
+			disable_filetype = { "TelescopePrompt", "spectre_panel" },
+			fast_wrap = {
+				chars = { "{", "[", "(", '"', "'" },
+				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+			},
+		})
+	end,
 }
-
